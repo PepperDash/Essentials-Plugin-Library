813ae9efe09f87d29c7c73526c2a09aaedd5384c
jtalborough <jalborough@pepperdash.com>
2023-09-01 09:14:19 -0400

    feature: additional debugging
    fix: branch depending on mode
    fix: continue when exception is hit

--- .github/scripts/gather_repo_urls.py
+++ .github/scripts/gather_repo_urls.py
@@ -1,35 +1,43 @@
 from github import Github
 import os
 import re
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 def find_interfaces(repo, path=""):
+    logging.debug(f"Searching for interfaces in repo: {repo.name}, path: {path}")
     interfaces = []
     try:
         contents = repo.get_contents(path, ref="main")
         for content in contents:
             if content.type == "dir":
+                logging.debug(f"Found directory: {content.path}")
                 interfaces.extend(find_interfaces(repo, content.path))
             elif content.name.endswith('.cs'):
+                logging.debug(f"Found C# file: {content.name}")
                 file_content = repo.get_contents(content.path).decoded_content.decode()
-                matches = re.findall(r'public class [A-Za-z0-9_]+ : ([A-Za-z0-9_,\s]+)', file_content)
+                matches = re.findall(r'public (abstract )?class [A-Za-z0-9_]+ : ([A-Za-z0-9_,\s]+)', file_content)
                 for match in matches:
-                    for item in match.split(","):
+                    for item in match[1].split(","):
                         item = item.strip()
                         if item.startswith("I"):
                             interfaces.append(item)
     except Exception as e:
-        print(f"Error while finding interfaces in {repo.name}: {e}")
+        logging.error(f"Error while finding interfaces in {repo.name}: {e}")
+        return interfaces  # Return whatever interfaces were found before the exception
 
     return list(set(interfaces))  # Remove duplicates
 
-
 def generate_markdown_file(repos):
+    logging.debug("Generating markdown file.")
     with open('README.md', 'w') as file:
         file.write("# Essentials Plugin Library\n\n")
         file.write("| Repository                          | Visibility     | Current Release | Interfaces |\n")
         file.write("|-------------------------------------|----------------|-----------------|------------|\n")
         for repo in sorted(repos, key=lambda x: x.name):
             if repo.name.startswith('epi-'):
+                logging.debug(f"Processing repo: {repo.name}, Public: {not repo.private}")
                 visibility = "Public" if not repo.private else "Internal"
                 releases = repo.get_releases()
                 current_release = "N/A"
@@ -44,12 +52,13 @@ def generate_markdown_file(repos):
                 file.write(f"| [{repo.name}]({repo.html_url}) | {visibility} | {current_release} | {interfaces_str} |\n")
 
 def main():
+    logging.debug("Starting script.")
     g = Github(os.getenv('GITHUB_TOKEN'))
     org = g.get_organization(os.getenv('ORG_NAME'))
 
     repos = list(org.get_repos(type='all'))  # Fetch all types of repositories
 
-    print(f"Number of repos before filtering: {len(repos)}")  # Debugging line
+    logging.debug(f"Number of repos before filtering: {len(repos)}")  # Debugging line
 
     generate_markdown_file(repos)
 
